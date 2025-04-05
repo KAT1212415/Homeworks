57712424dc1ea36acd08dd90ec4c928e835a0184
e2024024@gmail.com <e2024024@gmail.com>
2025-04-05 04:02:44 +0300

    ДЗ 7

---
+++ Task7_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину стороны куба");
+            double edgeLength = Convert.ToDouble(Console.ReadLine()); ;
+            CalcCube(edgeLength, out double volume, out double surfaceArea);
+            
+        }
+        static void CalcCube(double edgeLength, out double volume, out double surfaceArea)
+        {
+            volume = edgeLength * edgeLength * edgeLength;
+            surfaceArea = 6 * edgeLength * edgeLength;
+            Console.WriteLine($"Объем куба : {volume}");
+            Console.WriteLine($"Площадь поверхности:{surfaceArea}");
+        }
+    }
+}
---
+++ Task7_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("число элементов массива");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[a];
+            Console.WriteLine("Введите числа");
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine($"введите {i + 1} элемент ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            PrintNumbers(numbers, true);
+        }
+        static void PrintNumbers(int[] numbers, bool reverse = false)
+        {
+            if (reverse == false)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+            else
+            {
+                for (int i = numbers.Length - 1; i >= 0; i--)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+        }
+    }
+}
