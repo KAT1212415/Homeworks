c82e3360067506544713596ddd9aa4b89d1c0cb2
e2024024@gmail.com <e2024024@gmail.com>
2025-09-22 03:47:48 +0300

    2.10 MVVM    ДЗ

---
+++ Wpf2105/ColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Wpf2105
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is "Успешно!") ? Brushes.Green : Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
